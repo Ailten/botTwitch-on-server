390074973e4b58979c9841a22d1b9ff064581ce6
Ailten <faouzi26b@live.fr>
2025-09-21 19:53:52 +0200

    make bot run async

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from classFolder.TwitchBot import TwitchBot
 bot = TwitchBot()
 bot.run()
 
+# mark process for kill easilly.
+with open(f"{bot.pathFolder}/main.pid", "w") as f:
+    f.write(str(os.getpid()))
+
+# execute in background.
+# nohup python3 <path>/mon_script.py &
+
+# kill the background execution.
+# kill $(cat <path>/main.pid)
