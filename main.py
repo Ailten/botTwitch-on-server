e7539bc5d834734f23baadbadded5c6a4be3f393
Ailten <faouzi26b@live.fr>
2025-09-22 13:51:18 +0200

    edit ignore and main for using name set as backend process

--- main.py
+++ main.py
@@ -3,15 +3,3 @@ import os
 
 bot = TwitchBot()
 bot.run()
-
-# mark process for kill easilly.
-pathPid = f"{bot.pathFolder}/main.pid"
-if os.path.isfile(pathPid):
-    with open(pathPid, "w") as f:
-        f.write(str(os.getpid()))
-
-# execute in background.
-# nohup python3 <path>/mon_script.py &
-
-# kill the background execution.
-# kill $(cat <path>/main.pid)
