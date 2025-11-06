f395c606d382bfc0b4f21b263bee44d807ecdbee
Ailten <faouzi26b@live.fr>
2025-11-06 13:04:04 +0100

    re try obs web socket

--- classFolder/commandes/AudioUwu.py
+++ classFolder/commandes/AudioUwu.py
@@ -7,12 +7,12 @@ class AudioUwu(Commande):
 
     # the execution of commande.
     async def execute(self, client, message):
-        pass
+        #pass
 
         # todo : inclure an enum in block web socket for eatch type of call ?.
         # or a way to ask a specifique execution with parameters.
 
-        #obs = ObsWebSocket()
-        #obs.connect(client)
-        #obs.playAudio("uwu")  # bug hear -> can't print object with all details.
-        #obs.close()  # bug hear -> client is NoneType (connection to obs fail), and bug when try to close.
+        obs = ObsWebSocket()
+        obs.connect(client)
+        obs.playAudio("uwu")  # bug hear -> can't print object with all details.
+        obs.close()  # bug hear -> client is NoneType (connection to obs fail), and bug when try to close.
